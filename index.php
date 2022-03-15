82d4b3301b3b2d9b5793437f0ed909ca87cdc5fd
Thomas DUFOUR <thomas.dufour57400@gmail.com>
2022-03-15 16:48:04 +0100

    Left navbar added

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Roboto:wght@300;700&display=swap" rel="stylesheet">
+    <title>Thomas DUFOUR</title>
+</head>
+<body>
+        <div class="left-side">
+            <div class="profil-picture">
+
+            </div>
+            <div class="nom-prenom">
+                <h1>DUFOUR Thomas</h1>
+                <h2>Développeur Web</h2>
+            </div>
+            <div class="menu">
+                <ul class="flex-menu">
+                    <li class="flex-item"><a href="sdfsdf" class="link-item">About</a></li>
+                    <li class="flex-item"><a href="sdfsdf" class="link-item">Home</a></li>
+                    <li class="flex-item"><a href="sdfsdf" class="link-item">Services</a></li>
+                    <li class="flex-item"><a href="sdfsdf" class="link-item">Skills</a></li>
+                    <li class="flex-item"><a href="sdfsdf" class="link-item">Education</a></li>
+                </ul>
+            </div>
+            <div class="copyright">
+                ©Copyright ©2022 All right reserved
+            </div>
+        </div>
+        <div class="right-side">
+
+        </div>
+</body>
+</html>
