a01806517cdffa4155144b0e2556c6e228c70048
Thomas DUFOUR <thomas.dufour57400@gmail.com>
2022-03-17 22:28:38 +0100

    HTML structure

--- index.php
+++ index.php
@@ -11,33 +11,74 @@
     <link rel="stylesheet" href="src/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Roboto:wght@300;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&family=Roboto:wght@300;700&family=Raleway&display=swap" rel="stylesheet">
+    <script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
     <title>Thomas DUFOUR</title>
 </head>
 <body>
-        <div class="left-side">
+        <div class="left-side" style="display: flex; flex-direction: column">
             <div class="profil-picture">
 
             </div>
             <div class="nom-prenom">
                 <h1>DUFOUR Thomas</h1>
-                <h2>Développeur Web</h2>
             </div>
             <div class="menu">
                 <ul class="flex-menu">
                     <li class="flex-item"><a href="index.php" class="link-item selected-page">Home</a></li>
-                    <li class="flex-item"><a href="#" class="link-item">About</a></li>
-                    <li class="flex-item"><a href="#" class="link-item">Services</a></li>
+                    <li class="flex-item"><a href="#about" class="link-item">About</a></li>
+                    <li class="flex-item"><a href="#" class="link-item">BTS SIO</a></li>
                     <li class="flex-item"><a href="#" class="link-item">Skills</a></li>
                     <li class="flex-item"><a href="#" class="link-item">Education</a></li>
                 </ul>
             </div>
-            <div class="copyright">
-                ©Copyright ©2022 All right reserved
+            <div class="reseau">
+                <ul class="line-social">
+                    <li><a href="https://github.com/Krowv"><span class="iconify icone" data-icon="brandico:linkedin-rect"></span></a></li>
+                    <li><a href="https://www.linkedin.com/in/thomas-dufour-885a75205/"><span class="iconify icone" data-icon="akar-icons:github-fill"></span></a></li>
+                </ul>
             </div>
         </div>
+        <div class="copyright">
+            ©Copyright ©2022 <strong>DUFOUR Thomas</strong>
+        </div>
         <div class="right-side">
-
+            <div class="présentation">
+                <p>Thomas DUFOUR</p>
+                <br>
+                <p>Portfolio</p>
+            </div>
+        </div>
+        <div class="presentation">
+            <div class="presentation-title">
+                <h2 id="about">About</h2>
+                <hr class="title-subtitle">
+            </div>
+            <div class="flex-presentation">
+                <div class="content-img-presentation">
+                    <img src="assets/pdp.png" alt="">
+                </div>
+                <div class="content-text-presentation">
+                    <h3>Presentation</h3>
+                    <hr class="title-subtitle">
+                    <p>Hi, my name is DUFOUR Thomas alternant student in second year for the BTS SIO (Services Informatiques aux Organisations) at IRIS school in the Mediaschool campus. I've choose SLAM option (Solutions Logicielles et Application Métier) to become a web developer.</p>
+                    <p>Hi, my name is DUFOUR Thomas alternant student in second year for the BTS SIO (Services Informatiques aux Organisations) at IRIS school in the Mediaschool campus. I've choose SLAM option (Solutions Logicielles et Application Métier) to become a web developer.</p>
+                    <p>Hi, my name is DUFOUR Thomas alternant student in second year for the BTS SIO (Services Informatiques aux Organisations) at IRIS school in the Mediaschool campus. I've choose SLAM option (Solutions Logicielles et Application Métier) to become a web developer.</p>
+                    <p>Hi, my name is DUFOUR Thomas alternant student in second year for the BTS SIO (Services Informatiques aux Organisations) at IRIS school in the Mediaschool campus. I've choose SLAM option (Solutions Logicielles et Application Métier) to become a web developer.</p>
+                    <br>
+                    <h3>What I want to become</h3>
+                    <hr class="title-subtitle">
+                    <p>As i said before, i really want to become a web developer. My favorite languages are :</p>
+                    <ul>
+                        <li>- PHP (For symfony and Laravel)</li>
+                        <li>- Javascript (With Vue JS)</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="presentation-bts">
+                <h2>What's the BTS SIO</h2>
+                <hr class="title-subtitle">
+            </div>
         </div>
 </body>
 </html>
